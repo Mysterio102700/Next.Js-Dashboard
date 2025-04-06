6b8bf0c3eb0c6af8e9004df223efcc58029db10c
Remanth <hulk102700@gmail.com>
2025-04-06 23:12:58 +0530

    initial

---
+++ app/ui/fonts.ts
@@ -0,0 +1,10 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-lusitana",
+  weight: ["400", "700"],
+});
